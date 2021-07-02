760d82957628d4010af4fe1087951e7a97302082
kenyan-coder <scamsours@gmail.com>
2021-07-02 16:13:44 +0300

    create calculation functions

---
+++ index.js
@@ -0,0 +1,21 @@
+let num1 = 8;
+let num2 = 2;
+document.getElementById("num1").textContent=num1;
+document.getElementById("num2").textContent=num2;
+
+// Addition function here
+function add(){
+    num1 + num2
+}
+// Subtract function here
+function subtract(){
+    num1 -num2
+}
+// Divide function here
+function divide(){
+    num1/num2
+}
+// Multiply function here
+function Multiply(){
+    num1*num2
+}
