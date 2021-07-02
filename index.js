2f6a5386339213d7c6b5a9c18a52dcaff2b47a8d
kenyan-coder <scamsours@gmail.com>
2021-07-02 16:19:27 +0300

    render the calculated result

--- index.js
+++ index.js
@@ -2,20 +2,26 @@ let num1 = 8;
 let num2 = 2;
 document.getElementById("num1").textContent=num1;
 document.getElementById("num2").textContent=num2;
+let sumEl= document.getElementById("sum-el");
 
 // Addition function here
 function add(){
-    num1 + num2
+    let total= num1 + num2
+    sumEl.textContent=total;
+
 }
 // Subtract function here
 function subtract(){
-    num1 -num2
+    let total=num1 -num2;
+    sumEl.textContent=total;
 }
 // Divide function here
 function divide(){
-    num1/num2
+   let total= num1/num2
+   sumEl.textContent=total;
 }
 // Multiply function here
 function Multiply(){
-    num1*num2
+    let total =num1*num2
+    sumEl.textContent=total;
 }
